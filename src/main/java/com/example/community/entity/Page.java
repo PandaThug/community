26ec1577aea4c4d3b26fe913fa3a71a65f5b9c8f
CDConlyred <1746904635@qq.com>
2022-05-28 17:29:47 +0800

    2022/5/28 add

---
+++ src/main/java/com/example/community/entity/Page.java
@@ -0,0 +1,83 @@
+package com.example.community.entity;
+
+
+import lombok.*;
+
+/**
+ * 封装分页相关的信息
+ */
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Page {
+
+    // 当前的页码
+    private Integer current = 1;
+    // 显示的上限
+    private Integer limit = 10;
+    // 数据总数（用于计算总的页数）
+    private Integer rows;
+    // 查询路径（用于复用分页链接）
+    private String path;
+
+    public void setCurrent(int current) {
+        if (current >= 1) {
+            this.current = current;
+        }
+    }
+
+    public void setLimit(int limit) {
+        if (limit >= 1 && limit <= 100) {
+            this.limit = limit;
+        }
+    }
+
+    public void setRows(int rows) {
+        if (rows >= 0) {
+            this.rows = rows;
+        }
+    }
+
+    /**
+     * 获取当前页的起始行
+     * @return
+     */
+    public int getOffset() {
+        return (current - 1) * limit;
+    }
+
+    /**
+     * 获取总的页数
+     * @return
+     */
+    public int getTotal() {
+        if (rows % limit == 0) {
+            return rows / limit;
+        } else {
+            return rows / limit + 1;
+        }
+    }
+
+    /**
+     * 获取起始页码
+     * @return
+     */
+    public int getFrom() {
+        int from = current - 2;
+        return from < 1 ? 1 : from;
+    }
+
+    /**
+     * 获取结束页码
+     * @return
+     */
+    public int getTo() {
+        int to = current + 2;
+        int total = getTotal();
+        return to > total ? total : to;
+    }
+
+}
