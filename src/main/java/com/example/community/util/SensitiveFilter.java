404f31fe3177ff0efa31c2c187b4a2eae5fe8f6d
CDConlyred <1746904635@qq.com>
2022-06-05 18:19:16 +0800

    2022/6/5 add

--- src/main/java/com/example/community/util/SensitiveFilter.java
+++ src/main/java/com/example/community/util/SensitiveFilter.java
@@ -3,6 +3,8 @@ package com.example.community.util;
 import com.mysql.cj.log.Log;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+/**
+ * 过滤敏感词
+ *  By:熊猫打手
+ */
 @Component
 public class SensitiveFilter {
 
@@ -59,6 +65,68 @@ public class SensitiveFilter {
                 tempNode.setKeywordEnd(true);
             }
         }
+    }
+
+    /**
+     * 过滤敏感词
+     * @param text 待过滤的文本
+     * @return 过滤后的文本
+     */
+    public String filter(String text) {
+        if (StringUtils.isBlank(text)) {
+            return null;
+        }
+        // 指针1
+        TrieNode tempNode = rootNode;
+        // 指针2
+        int begin = 0;
+        // 指针3
+        int position = 0;
+        // 结果字符串
+        StringBuilder sb = new StringBuilder();
+        while (position < text.length()) {
+            char c = text.charAt(position);
+            // 跳过符号
+            if (isSymbol(c)) {
+                // 若指针1处于根节点，将此符号计入结果，让指针2向下走一步
+                if (tempNode == rootNode) {
+                    sb.append(c);
+                    begin++;
+                }
+                // 无论符号在开头或中间，指针3都向下走一步
+                position++;
+                continue;
+            }
+            // 检查下级节点
+            tempNode = tempNode.getSubNode(c);
+            if (tempNode == null) {
+                // 以begin为开头的字符串不是敏感词
+                sb.append(text.charAt(begin));
+                // 进入下一个位置
+                position = ++begin;
+                // 重新指向根节点
+                tempNode = rootNode;
+            } else if (tempNode.isKeywordEnd()) {
+                // 发现敏感词，将begin~position字符串替换掉
+                sb.append(REPLACEMENT);
+                // 进入下一个位置
+                begin = ++position;
+                // 重新指向根节点
+                tempNode = rootNode;
+            } else {
+                // 检查下一个字符
+                position++;
+            }
+        }
+        // 将最后一批字符计入结果
+        sb.append(text.substring(begin));
+        return sb.toString();
+    }
+
+    // 判断是否为符号
+    private boolean isSymbol(Character c) {
+        // 0x2E80~0x9FFF 是东亚文字范围
+        return !CharUtils.isAsciiAlphanumeric(c) && (c < 0x2E80 || c > 0x9FFF);
     }
 
     // 前缀树
