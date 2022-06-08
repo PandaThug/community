004c0eba92eae08ea1ac2996878172def362e2b0
CDConlyred <1746904635@qq.com>
2022-06-08 23:33:49 +0800

    2022/6/8 整合Redis

---
+++ src/main/java/com/example/community/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.community.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class RedisConfig {
+
+    @Resource
+    RedisConnectionFactory factory;
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        // 设置key的序列化方式
+        template.setKeySerializer(RedisSerializer.string());
+        // 设置value的序列化方式
+        template.setValueSerializer(RedisSerializer.json());
+        // 设置hash的key的序列化方式
+        template.setHashKeySerializer(RedisSerializer.string());
+        // 设置hash的value的序列化方式
+        template.setHashValueSerializer(RedisSerializer.json());
+
+        template.afterPropertiesSet();
+        return template;
+
+    }
+}
