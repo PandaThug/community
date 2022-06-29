aa0da369dae4e2630c94252b0aa721dd83285b8a
CDConlyred <1746904635@qq.com>
2022-06-29 12:25:02 +0800

    2022/6/29 测试Elasticsearch，仍有问题

--- src/test/java/com/example/community/ElasticsearchTest.java
+++ src/test/java/com/example/community/ElasticsearchTest.java
@@ -3,11 +3,18 @@ package com.example.community;
 import com.example.community.dao.DiscussPostMapper;
 import com.example.community.dao.elasticsearch.DiscussPostRepository;
 import com.example.community.entity.DiscussPost;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -34,5 +41,27 @@ public class ElasticsearchTest {
         post.setContent("熊猫打手");
         discussPostRepository.save(post);
     }
+
+    @Test
+    public void testSearchRepository() {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("关键字", "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                )
+                .build();
+        Page<DiscussPost> page = discussPostRepository.search(searchQuery);
+        System.out.println(page.getTotalElements());
+        System.out.println(page.getTotalPages());
+        System.out.println(page.getNumber());
+        System.out.println(page.getSize());
+        page.forEach(System.out::println);
+    }
+
 
 }
