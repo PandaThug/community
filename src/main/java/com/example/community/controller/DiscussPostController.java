8fb480a8248f06f1c7896316ed91be10a33f310a
CDConlyred <1746904635@qq.com>
2022-07-04 12:53:52 +0800

    2022/7/4 开发社区搜索功能

--- src/main/java/com/example/community/controller/DiscussPostController.java
+++ src/main/java/com/example/community/controller/DiscussPostController.java
@@ -1,9 +1,7 @@
 package com.example.community.controller;
 
-import com.example.community.entity.Comment;
-import com.example.community.entity.DiscussPost;
-import com.example.community.entity.Page;
-import com.example.community.entity.User;
+import com.example.community.entity.*;
+import com.example.community.event.EventProducer;
 import com.example.community.service.CommentService;
 import com.example.community.service.DiscussPostService;
 import com.example.community.service.LikeService;
@@ -35,6 +33,8 @@ public class DiscussPostController implements CommunityConstant {
     private CommentService commentService;
     @Autowired
     private LikeService likeService;
+    @Autowired
+    private EventProducer eventProducer;
 
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
@@ -48,6 +48,13 @@ public class DiscussPostController implements CommunityConstant {
         post.setContent(content);
         post.setCreateTime(new Date());
         discussPostService.addDiscussPost(post);
+        // 触发发帖事件
+        Event event = new Event()
+                .setTopic(TOPIC_PUBLISH)
+                .setUserId(user.getId())
+                .setEntityType(ENTITY_TYPE_POST)
+                .setEntityId(post.getId());
+        eventProducer.fireEvent(event);
         return CommunityUtil.getJSONString(0, "发布成功！");
     }
 
--- src/main/java/com/example/community/event/EventConsumer.java
+++ src/main/java/com/example/community/event/EventConsumer.java
@@ -1,11 +1,15 @@
 package com.example.community.event;
 
 import com.alibaba.fastjson.JSONObject;
+import com.example.community.entity.DiscussPost;
 import com.example.community.entity.Event;
 import com.example.community.entity.Message;
+import com.example.community.service.DiscussPostService;
+import com.example.community.service.ElasticsearchService;
 import com.example.community.service.MessageService;
 import com.example.community.util.CommunityConstant;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.checkerframework.checker.units.qual.A;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +26,10 @@ public class EventConsumer implements CommunityConstant {
     private static final Logger logger = LoggerFactory.getLogger(EventConsumer.class);
     @Autowired
     private MessageService messageService;
+    @Autowired
+    private DiscussPostService discussPostService;
+    @Autowired
+    private ElasticsearchService elasticsearchService;
 
     @KafkaListener(topics = {TOPIC_COMMENT, TOPIC_LIKE, TOPIC_FOLLOW})
     public void HandleCommentMessage(ConsumerRecord record) {
@@ -52,4 +60,21 @@ public class EventConsumer implements CommunityConstant {
         message.setContent(JSONObject.toJSONString(content));
         messageService.addMessage(message);
     }
+
+    // 消费发帖事件
+    @KafkaListener(topics = {TOPIC_PUBLISH})
+    public void handlePublishMessage(ConsumerRecord record) {
+        if (record == null || record.value() == null) {
+            logger.error("消息的内容为空!");
+            return;
+        }
+        Event event = JSONObject.parseObject(record.value().toString(), Event.class);
+        if (event == null) {
+            logger.error("消息格式错误!");
+            return;
+        }
+        DiscussPost post = discussPostService.findDiscussPost(event.getEntityId());
+        elasticsearchService.saveDiscussPost(post);
+    }
+
 }
