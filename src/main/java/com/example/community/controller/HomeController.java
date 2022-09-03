cfd2e07de24b330c69eddeb7f16f6a5e5f978aba
CDConlyred <1746904635@qq.com>
2022-09-03 21:58:45 +0800

    2022/9/3 使用SpringSecurity进行权限管理

--- src/main/java/com/example/community/controller/HomeController.java
+++ src/main/java/com/example/community/controller/HomeController.java
@@ -56,4 +56,9 @@ public class HomeController implements CommunityConstant {
         return "/error/500";
     }
 
+    @RequestMapping(path = "/denied", method = RequestMethod.GET)
+    public String getDeniedPage() {
+        return "/error/404";
+    }
+
 }
--- src/main/java/com/example/community/entity/User.java
+++ src/main/java/com/example/community/entity/User.java
@@ -1,15 +1,20 @@
 package com.example.community.entity;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     private Integer id;
     private String username;
@@ -21,5 +26,46 @@ public class User {
     private String activationCode;
     private String headerUrl;
     private Date createTime;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> list = new ArrayList<>();
+        list.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                switch (type) {
+                    case 1:
+                        return "ADMIN";
+                    default:
+                        return "USER";
+                }
+            }
+        });
+        return list;
+    }
+
+    // true:账号未过期
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // true:账号未锁定
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // true:凭证未过期
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // true:账号可用
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
