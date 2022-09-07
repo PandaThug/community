cc664cd9d4fb088552f792fb281a0a7a2694ee72
CDConlyred <1746904635@qq.com>
2022-09-07 22:21:39 +0800

    2022/9/7 热帖排行

--- src/main/java/com/example/community/controller/HomeController.java
+++ src/main/java/com/example/community/controller/HomeController.java
@@ -30,7 +30,7 @@ public class HomeController implements CommunityConstant {
     private LikeService likeService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
-    public String getIndexPage(Model model, Page page, @RequestParam(name = "orderMode", defaultValue = 0) int orderMode) {
+    public String getIndexPage(Model model, Page page, @RequestParam(name = "orderMode", defaultValue = "0") int orderMode) {
         // 方法调用前，SpringMVC会自动实例化Model和Page，并将Page注入Model。
         // 所以，在Thymeleaf中可以直接访问Page对象中的数据。
         page.setRows(discussPostService.findDiscussPostRows(0));
